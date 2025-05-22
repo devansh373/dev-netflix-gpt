e18dbcc37ff18f63fdfc8c494ace43f2501f0164
Devansh Raghav <Devanshraghavsn@gmail.com>
2025-05-22 21:45:20 +0530

    implemented toggle between sign in and sign up PAGES

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const switchSignInSignUp = () => {
+    setIsSignUp((prev) => !prev);
+  };
   return (
     <div className="bg-[url(https://assets.nflxext.com/ffe/siteui/vlv3/914ad279-199e-4095-9c10-2409dc9e5e1b/web/IN-en-20250519-TRIFECTA-perspective_8f1ca896-9e49-4a4e-90f0-22fc49650bd9_large.jpg)] bg-cover bg-no-repeat bg-center h-screen">
       <Header />
@@ -12,23 +16,35 @@ const Login = () => {
 
       <form
         action=""
-        className="absolute top-1/2 left-1/2 w-96 h-[500px] bg-black/85  rounded-lg p-10 flex flex-col items-center -translate-x-1/2 -translate-y-1/2 text-white login-form"
+        className="absolute top-1/2 left-1/2 w-96 h-auto bg-black/85  rounded-lg p-10 flex flex-col items-center -translate-x-1/2 -translate-y-1/2 text-white login-form transition delay-200 ease-in-out"
         method="POST"
         onSubmit={(e) => {
-            e.preventDefault();
-            const formData = new FormData(e.target);
-            const data = Object.fromEntries(formData.entries());
-            console.log(data);
-            }
-        }
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          const data = Object.fromEntries(formData.entries());
+          console.log(data);
+        }}
       >
-        <h1 className="w-full font-bold text-2xl text-center m-3">Login</h1>
+        <h1 className="w-full font-bold text-2xl text-center m-3">{isSignUp?"Sign Up":"Sign In"}</h1>
+        {isSignUp && (
+          <>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="abc@example.com"
+              required
+            />
+          </>
+        )}
         <label htmlFor="email">Email</label>
         <input
           type="email"
           name="email"
           id="email"
           placeholder="abc@example.com"
+          required
         />
         <label htmlFor="password">Password</label>
         <input
@@ -36,15 +52,32 @@ const Login = () => {
           name="password"
           id="password"
           placeholder="Password"
+          required
         />
+        {isSignUp&&<>
         <label htmlFor="confirm-password">Confirm Password</label>
         <input
           type="password"
           name="confirm-password"
           id="confirm-password"
           placeholder="Confirm Password"
-        />
-        <button type="submit" className="w-36 p-2 text-xl border border-gray-400 rounded-lg mt-4 cursor-pointer hover:bg-gray-800">Submit</button>
+          required
+          />
+          </>
+          }
+
+        <button
+          type="submit"
+          className="w-36 p-2 text-xl border border-gray-400 rounded-lg mt-4 cursor-pointer hover:bg-gray-800"
+        >
+          Submit
+        </button>
+        <p
+          className="p-4 mt-2 w-full hover:underline cursor-pointer"
+          onClick={switchSignInSignUp}
+        >
+          {isSignUp?"Already have an account? Sign In Now":"New to Netflix? Sign Up Now"}
+        </p>
       </form>
     </div>
   );
