58319ad6ac4b39322217fdcf1a0d55821fdffb4a
Devansh Raghav <Devanshraghavsn@gmail.com>
2025-05-24 15:15:33 +0530

    registered to tmdb and made api call to tmdb api

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { TMDB_Api_Options } from '../utils/constants'
 
 const Browse = () => {
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/now_playing?page=1",TMDB_Api_Options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+  
+   
+  }, [])
+  
   return (
     <div>
       <Header/>
